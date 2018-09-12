4df3f519a219d3e32c5eec236c424c037567be70
jjfang97 <jjfang@ufl.edu>
2018-09-12 00:39:15 -0400

    add listings and index

--- listingController.js
+++ listingController.js
@@ -7,8 +7,31 @@ angular.module('listings').controller('ListingsController', ['$scope', 'Listings
       Implement these functions in the controller to make your application function 
       as described in the assignment spec. 
      */
-    $scope.addListing = function() {};
-    $scope.deleteListing = function(index) {};
-    $scope.showDetails = function(index) {};
+    $scope.addListing = function() 
+    {
+        $scope.listings.push
+        ({
+            index:$scope.listings.length,
+            name: $scope.nName,
+            code: $scope.nCode,
+            address: $scope.nAddress,
+            coordinates: 
+            {
+                latitude: $scope.nLatitude,
+                longitude: $scope.nLongitude
+            }
+        });
+    };
+
+    $scope.deleteListing = function(index) 
+    {
+        var delIndex = $scope.listings.indexOf(item);
+        $scope.listings.splice(delIndex,1);
+    };
+    $scope.showDetails = function(index) 
+    {
+        var showIndex = $scope.listings.indexOf(item);
+        $scope.detailedInfo = Listings[showIndex];
+    };
   }
 ]);
